744725636fb512bb7965b3698b56ffacb70ec481
kimbom <springnode@gmail.com>
2016-02-24 21:38:51 +0900

    headline

--- main.pl
+++ main.pl
@@ -7,14 +7,17 @@ require 'login/info.pl';
 my $q=new CGI;
 my $c_id=GetCookieId($q);
 
+my $headline_css_file;
 my $webmenu="";
 if($c_id){	#login
-	$webmenu.='<a href="login/logout.pl"><img src="css/logout.jpg"/></a>';
-	$webmenu.='<a href="login/user_info.pl"><img src="css/setting.jpg"/></a>';
 	$webmenu.='<a href="login/user_info.pl"><img src="css/information.jpg"/></a>';
+	$webmenu.='<a href="login/user_info.pl"><img src="css/setting.jpg"/></a>';
+	$webmenu.='<a href="login/logout.pl"><img src="css/logout.jpg"/></a>';
+	$headline_css_file='css/headline_login.css';
 }else{		
-	$webmenu.='<a href="login/signup.pl"><img src="css/joinus.jpg"/></a>';
 	$webmenu.='<a href="login/login.pl"><img src="css/signin.jpg"/></a>';
+	$webmenu.='<a href="login/signup.pl"><img src="css/joinus.jpg"/></a>';
+	$headline_css_file='css/headline_logout.css';
 }
 
 #==============================WRITE PERL CGI==============================
@@ -22,6 +25,7 @@ print $q->header(-charset=>"UTF-8");
 print <<EOF
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="$headline_css_file">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
